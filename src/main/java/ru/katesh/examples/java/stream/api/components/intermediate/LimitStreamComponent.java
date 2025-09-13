4046267e69c9333d636703017acdf792c11364d8
dmko <kotyshok@yandex.ru>
2025-09-13 22:55:43 +0300

    Добавил примеры с limit, sorted и терминальными операциями

--- src/main/java/ru/katesh/examples/java/stream/api/components/intermediate/LimitStreamComponent.java
+++ src/main/java/ru/katesh/examples/java/stream/api/components/intermediate/LimitStreamComponent.java
@@ -1,4 +1,29 @@
 package ru.katesh.examples.java.stream.api.components.intermediate;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Метод limit возвращает новый поток, содержащий не больше
+ */
 public class LimitStreamComponent {
+
+    /**
+     * Пример использования метода limit
+     */
+    public void limit() {
+        Integer[] arr = {1, 9, 2, 8, 3, 7, 4, 6, 5, 13, 14, 15, 10, 11, 12};
+        List<Integer> list = Arrays.stream(arr)
+                .limit(5)
+                .toList();
+        System.out.println("Возвращаем не более 5 элементов = " + list);
+    }
+
+    /**
+     * Демонстрация работы метода sorted
+     */
+    public static void main(String[] args){
+        LimitStreamComponent limitStreamComponent = new LimitStreamComponent();
+        limitStreamComponent.limit();
+    }
 }
--- src/main/java/ru/katesh/examples/java/stream/api/components/intermediate/SortedStreamComponent.java
+++ src/main/java/ru/katesh/examples/java/stream/api/components/intermediate/SortedStreamComponent.java
@@ -1,4 +1,41 @@
 package ru.katesh.examples.java.stream.api.components.intermediate;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Метод sorted в Java Stream API относится к промежуточным операциям и является stateful (он должен видеть весь
+ * поток, чтобы отсортировать).
+ */
 public class SortedStreamComponent {
+
+    /**
+     * Пример работы сортировки
+     */
+    public void sorted() {
+        Integer[] arr = {1, 9, 2, 8, 3, 7, 4, 6, 5, 13, 14, 15, 10, 11, 12};
+        List<Integer> list = Arrays.stream(arr)
+                .toList();
+        System.out.println("Массив отсортированный по возрастанию = " + list);
+    }
+
+    /**
+     * Пример работы сортировки с указанием comparator
+     */
+    public void sortedWithComparator() {
+        Integer[] arr = {1, 9, 2, 8, 3, 7, 4, 6, 5, 13, 14, 15, 10, 11, 12};
+        List<Integer> list = Arrays.stream(arr)
+                .sorted((o1, o2) -> o2 - o1)
+                .toList();
+        System.out.println("Массив отсортированный по убыванию = " + list);
+    }
+
+    /**
+     * Демонстрация работы метода sorted
+     */
+    public static void main(String[] args){
+        SortedStreamComponent sortedStreamComponent = new SortedStreamComponent();
+        sortedStreamComponent.sorted();
+        sortedStreamComponent.sortedWithComparator();
+    }
 }
