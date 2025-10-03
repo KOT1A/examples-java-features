746a2edffa34319cd89d319e732ca800acf12069
dmko <kotyshok@yandex.ru>
2025-10-03 13:55:42 +0300

    Добавил пример работы с enum

--- enum/src/main/java/ru/kot1a/examples/java/features/enumm/DayOfWeek.java
+++ enum/src/main/java/ru/kot1a/examples/java/features/enumm/DayOfWeek.java
@@ -1,11 +1,47 @@
 package ru.kot1a.examples.java.features.enumm;
 
+/**
+ * Как и у обычного класса у enum могут быть обычные поля, константы и статические поля, константы.
+ */
 public enum DayOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
+
+    MONDAY("Понедельник", "MON"),
+    TUESDAY("Вторник", "TUE"),
+    WEDNESDAY("Среда", "WED"),
+    THURSDAY("Четверг", "THU"),
+    FRIDAY("Пятница", "FRI"),
+    SATURDAY("Суббота", "SAT"),
+    SUNDAY("Воскресенье", "SUN");
+
+    static {
+        DAY_OF_WEEK_RU = "День недели: ";
+    }
+
+    public static final String DAY_OF_WEEK_RU;
+    public static String DAY_OF_WEEK_EN = "Day of week: ";
+
+    private String russianNameOfWeek;
+    private final String abbr;
+
+    DayOfWeek(String name, String abbr) {
+        this.russianNameOfWeek = name;
+        this.abbr = abbr;
+    }
+
+    public String getRussianNameOfWeek(){
+        return russianNameOfWeek;
+    }
+
+    public void setRussianNameOfWeek(String russianNameOfWeek){
+        this.russianNameOfWeek = russianNameOfWeek;
+    }
+
+    public String getAbbr(){
+        return abbr;
+    }
+
+    @Override
+    public String toString() {
+        return DAY_OF_WEEK_RU + this.russianNameOfWeek + "[" + this.abbr + "]";
+    }
 }
--- enum/src/main/java/ru/kot1a/examples/java/features/enumm/EnumExampleApp.java
+++ enum/src/main/java/ru/kot1a/examples/java/features/enumm/EnumExampleApp.java
@@ -2,7 +2,58 @@ package ru.kot1a.examples.java.features.enumm;
 
 public class EnumExampleApp {
 
-    public static void main(String[] args){
+    /**
+     * Встроенный метод, который преобразует строку в enum
+     */
+    public void valueOf() {
+        DayOfWeek monday = DayOfWeek.valueOf("MONDAY");
+        System.out.println("valueOf - " + monday.getAbbr());
+    }
 
+    /**
+     * Встроенный метод, который возвращает имя enum
+     */
+    public void name() {
+        String tuesday = DayOfWeek.TUESDAY.name();
+        System.out.println("name - " + tuesday);
+    }
+
+    /**
+     * Встроенный метод, который возвращает порядковый номер enum
+     */
+    public void ordinal() {
+        int wednesday = DayOfWeek.WEDNESDAY.ordinal();
+        System.out.println("ordinal - " + wednesday);
+    }
+
+    /**
+     * Метод выводит все элементы enum
+     */
+    public void forEach() {
+        System.out.println("Выводим все элементы enum:");
+        for (DayOfWeek d : DayOfWeek.values()) {
+            System.out.println(d);
+        }
+    }
+
+    /**
+     * Встроенный метод, который возвращает порядковый номер enum
+     */
+    public void changeField() {
+        System.out.println("russianNameOfWeek - " + DayOfWeek.WEDNESDAY.getRussianNameOfWeek());
+        DayOfWeek.WEDNESDAY.setRussianNameOfWeek("Средулька");
+        System.out.println("russianNameOfWeek - " + DayOfWeek.WEDNESDAY.getRussianNameOfWeek());
+    }
+
+    /**
+     * Демонстрация работы с enum
+     */
+    public static void main(String[] args){
+        EnumExampleApp enumExampleApp = new EnumExampleApp();
+        enumExampleApp.valueOf();
+        enumExampleApp.name();
+        enumExampleApp.ordinal();
+        enumExampleApp.forEach();
+        enumExampleApp.changeField();
     }
 }
